92cf40bc9a6c8fd2889609b4ba7356e74f3be2dc
Matheus Correia <matheuscorreia1910@gmail.com>
2026-06-14 17:35:22 -0300

    feat: "render e adição de task em app.js"

--- src/js/app.js
+++ src/js/app.js
@@ -28,3 +28,72 @@ const taskList =
   document.getElementById("taskList");
 
 let currentFilter = "all";
+
+/* =========================
+   RENDER
+========================= */
+
+function render() {
+  let tasks =
+    taskManager.getAllTasks();
+
+  if (currentFilter !== "all") {
+    tasks =
+      taskManager.filterTasks(
+        currentFilter
+      );
+  }
+
+  const searchTerm =
+    searchInput.value.trim();
+
+  if (searchTerm) {
+    tasks = tasks.filter(task =>
+      task.title
+        .toLowerCase()
+        .includes(
+          searchTerm.toLowerCase()
+        )
+    );
+  }
+
+  ui.renderTasks(tasks);
+
+  ui.updateStats(
+    taskManager.getStats()
+  );
+}
+
+/* =========================
+   ADD TASK
+========================= */
+
+function addTask() {
+  const title =
+    taskInput.value.trim();
+
+  const priority =
+    prioritySelect.value;
+
+  if (!title) {
+    showToast(
+      "⚠️ Digite uma tarefa",
+      "#f59e0b"
+    );
+
+    return;
+  }
+
+  taskManager.addTask(
+    title,
+    priority
+  );
+
+  taskInput.value = "";
+
+  render();
+
+  showToast(
+    "✅ Tarefa adicionada"
+  );
+}
